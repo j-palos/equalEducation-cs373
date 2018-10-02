7ccec8cd17e02359322c07a78eb6128a48930ed4
Jesus Palos <jesus.rpalos@gmail.com>
2018-10-01 23:54:23 -0500

    Router setup, Header separated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './styles/carousel.css';
 import './styles/bootstrap-4.1.3-dist/css/bootstrap.min.css'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LandingPage from "./LandingPage";
 import ReactDOM from "react-dom";
+import SchoolPage from "./SchoolPage";
+import VolunteerPage from "./VolunteerPage";
+import CharityPage from "./CharityPage";
 // import AboutPage from './AboutPage';
 
 class App extends Component {
@@ -14,9 +17,12 @@ class App extends Component {
                 <BrowserRouter>
                     <Switch>
                          <Route exact path="/" component={LandingPage} />
-                        {/*<Route exact path="/about" component={AboutPage} />*/}
-                        {/* <Route path="/listing/:business" component={BusinessPage} /> */}
+                        <Route exact path="/school" component={SchoolPage} />
+                         {/*<Route path="/listing/:business" component={BusinessPage} /> */}
                         {/*<Route path={this.window.location}*/}
+                        <Route exact path={"/volunteer"} component={VolunteerPage} />
+                        <Route exact path={"/charity"} component={CharityPage} />
+                        <Route exact path={"/volunteer"} component={VolunteerPage} />
                          <Route component={LandingPage} />
                     </Switch>
                 </BrowserRouter>
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from "./Header";
 
 class LandingPage extends Component {
 
@@ -6,7 +7,70 @@ class LandingPage extends Component {
 
     render() {
         return (
-            <div>The landing page</div>
+            <div>
+                <Header />
+                <main role="main">
+                    <div id="root" />
+                    {/**/}
+                    <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                        <ol className="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to={0} className="active" />
+                            <li data-target="#myCarousel" data-slide-to={1} />
+                            <li data-target="#myCarousel" data-slide-to={2} />
+                        </ol>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img className="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide" />
+                                <div className="container">
+                                    <div className="carousel-caption text-left">
+                                        <h1>Example headline.</h1>
+                                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                                        <p><a className="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <img className="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide" />
+                                <div className="container">
+                                    <div className="carousel-caption">
+                                        <h1>Another example headline.</h1>
+                                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                                        <p><a className="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <img className="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide" />
+                                <div className="container">
+                                    <div className="carousel-caption text-right">
+                                        <h1>One more for good measure.</h1>
+                                        <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                                        <p><a className="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true" />
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true" />
+                            <span className="sr-only">Next</span>
+                        </a>
+                    </div>
+                    {/* Marketing messaging and featurettes
+================================================== */}
+                    {/* Wrap the rest of the page in another container to center all the content. */}
+                    <div className="container marketing">
+                    </div>{/* /.container */}
+                    {/* FOOTER */}
+                    <footer className="container">
+                        <p className="float-right"><a href="#">Back to top</a></p>
+                        <p>© Equal Education · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+                    </footer>
+                </main>
+            </div>
         );
     }
 }
