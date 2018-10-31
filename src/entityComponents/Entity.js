9013eeb4ac9fc7f939c2d6792107f085160505be
Jesus Palos <jesus.rpalos@gmail.com>
2018-10-30 20:46:58 -0500

    API hooked up

--- src/entityComponents/Entity.js
+++ src/entityComponents/Entity.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {EntityName} from './EntityName';
-import {Card, CardBody, CardDeck, CardImg, Container, Row} from 'reactstrap';
+import {Card, CardBody, CardColumns, CardImg, Container, Row} from 'reactstrap';
 import {EntityInfo} from "./EntityInfo";
 
 export const Entity = (props) => {
@@ -11,17 +11,18 @@ export const Entity = (props) => {
             <EntityName title={info.Name}/>
             <Row>
                 <Container>
-                    <CardDeck>
+                    <CardColumns className={'mx-auto'}>
                         <Card>
                             <CardImg top width="100%" src={info.Image} className={'img-fluid rounded'}/>
+                        </Card>
+                        <Card>
                             <CardBody>
                                 <EntityInfo info={info.Info}/>
                             </CardBody>
                         </Card>
-                    </CardDeck>
+                    </CardColumns>
                 </Container>
             </Row>
-            <Row></Row>
         </Container>
     );
 };
