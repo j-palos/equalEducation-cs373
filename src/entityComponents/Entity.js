395ba17ef38d5c42b32d8a325a1c4b6ef2963456
Jesus Palos <jesus.rpalos@gmail.com>
2018-10-30 21:17:54 -0500

    style fix

--- src/entityComponents/Entity.js
+++ src/entityComponents/Entity.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {EntityName} from './EntityName';
-import {Card, CardBody, CardGroup, CardImg, Container, Row} from 'reactstrap';
+import {Card, CardBody, CardImg, Col, Container, Row} from 'reactstrap';
 import {EntityInfo} from "./EntityInfo";
 
 export const Entity = (props) => {
@@ -11,16 +11,20 @@ export const Entity = (props) => {
             <EntityName title={info.Name}/>
             <Row>
                 <Container>
-                    <CardGroup className={'mx-auto'}>
+                    <Row>
+                        <Col sm={'4'}>
                         <Card>
                             <CardImg top width="100%" src={info.Image} className={'img-fluid rounded'}/>
                         </Card>
-                        <Card style={{width: '50%'}} className={'mx-auto'}>
+                        </Col>
+                        <Col sm={'8'}>
+                            <Card style={{width: '100%'}} className={'mx-auto'}>
                             <CardBody>
                                 <EntityInfo info={info.Info}/>
                             </CardBody>
                         </Card>
-                    </CardGroup>
+                        </Col>
+                    </Row>
                 </Container>
             </Row>
         </Container>
